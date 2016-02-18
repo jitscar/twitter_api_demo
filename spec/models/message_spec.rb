46276b7fe67607f44c9564e391235fe47e72e882
jitscar <jitscar@gmail.com>
2016-02-19 02:16:02 +0500

    test message db structure

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Message, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "db structure" do
+    it { is_expected.to have_db_column(:user_id).of_type(:integer) }
+    it { is_expected.to have_db_column(:content).of_type(:string) }
+    it { is_expected.to have_db_column(:like).of_type(:boolean) }
+    it { is_expected.to have_db_column(:created_at).of_type(:datetime) }
+    it { is_expected.to have_db_column(:updated_at).of_type(:datetime) }
+  end
 end
