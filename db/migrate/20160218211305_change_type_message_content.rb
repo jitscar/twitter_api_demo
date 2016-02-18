d43d1bd091b2249d66e979e0980417f8d0bc330e
jitscar <jitscar@gmail.com>
2016-02-19 02:17:18 +0500

    add change type message content migration

---
+++ db/migrate/20160218211305_change_type_message_content.rb
@@ -0,0 +1,5 @@
+class ChangeTypeMessageContent < ActiveRecord::Migration
+  def change
+    change_column :messages, :content, :string, null: false, limit: 140
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,17 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160217223214) do
+ActiveRecord::Schema.define(version: 20160218211305) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "messages", force: :cascade do |t|
     t.integer  "user_id"
-    t.text     "content",                    null: false
-    t.boolean  "like",       default: false
-    t.datetime "created_at",                 null: false
-    t.datetime "updated_at",                 null: false
+    t.string   "content",    limit: 140,                 null: false
+    t.boolean  "like",                   default: false
+    t.datetime "created_at",                             null: false
+    t.datetime "updated_at",                             null: false
   end
 
   add_index "messages", ["user_id"], name: "index_messages_on_user_id", using: :btree
