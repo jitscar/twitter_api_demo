b0bfe1b9779d827fc18204353466f8d4ab60e9eb
jitscar <jitscar@gmail.com>
2016-02-20 03:20:21 +0500

    adding and testing messages controller

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -1,2 +1,28 @@
-class Api::V1::MessagesController < Api::ApiController
+class API::V1::MessagesController < ApplicationController
+
+  def index
+    @messages = Message.all
+
+    respond_to do |format|
+      format.json { render json: @messages }
+    end
+  end
+
+  def create
+    @message = Message.new(message_params)
+
+    respond_to do |format|
+      if @message.save
+        format.json { render json: @message }
+      else
+        format.json { render json: @message.errors }
+      end
+    end
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:content, :user_id)
+  end
 end
--- spec/controllers/api/v1/messages_controller_spec.rb
+++ spec/controllers/api/v1/messages_controller_spec.rb
@@ -1,5 +1,41 @@
 require 'rails_helper'
 
-RSpec.describe Api::V1::MessagesController, type: :controller do
+RSpec.describe API::V1::MessagesController, type: :controller do
+  let(:valid_attributes) {
+    FactoryGirl.attributes_for(:message)
+  }
 
+  let(:invalid_attributes) {
+    { content: nil, user_id: nil }
+  }
+
+  describe "GET #index" do
+    it "returns http success" do
+      get :index, format: :json
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "POST #create" do
+    context "with valid params" do
+      it "creates a new Message" do
+        expect {
+          post :create, { message: valid_attributes, format: :json  }
+        }.to change(Message, :count).by(1)
+      end
+
+      it "assigns a newly created message as @message" do
+        post :create, { message: valid_attributes, format: :json  }
+        expect(assigns(:message)).to be_a(Message)
+        expect(assigns(:message)).to be_persisted
+      end
+    end
+
+    context "with invalid params" do
+      it "assigns a newly created but unsaved message as @message" do
+        post :create, { message: invalid_attributes, format: :json  }
+        expect(assigns(:message)).to be_a_new(Message)
+      end
+    end
+  end
 end
