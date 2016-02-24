98c20acf9138e7981431431da6b5c84e4cda288e
jitscar <jitscar@gmail.com>
2016-02-25 00:58:03 +0500

    correct method private

--- lib/utils/average_rating.rb
+++ lib/utils/average_rating.rb
@@ -3,9 +3,9 @@ module AverageRating
     user.update_attribute(:avg_rating, calculate_rating(user))
   end
 
-  private
-
   def self.calculate_rating(user)
     Message.where(user_id: user.id).sum(:favorites_count).to_f / user.messages_count.to_f
   end
+
+  private_class_method :calculate_rating
 end
