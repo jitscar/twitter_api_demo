3f8958de2cea1579b67f6e240890a0e481a4dc03
jitscar <jitscar@gmail.com>
2016-02-25 01:15:58 +0500

    correct message controller

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -1,36 +1,40 @@
-class API::V1::MessagesController < ApiController
-  def index
-    @messages = Message.all
+module API
+  module V1
+    class MessagesController < ApiController
+      def index
+        @messages = Message.all
 
-    respond_to do |format|
-      format.json { render json: @messages }
-    end
-  end
+        respond_to do |format|
+          format.json { render json: @messages }
+        end
+      end
 
-  def create
-    @message = Message.new(message_params)
+      def create
+        @message = Message.new(message_params)
 
-    respond_to do |format|
-      if @message.save
-        format.json { render json: @message, status: :created }
-      else
-        format.json { render json: @message.errors, status: :unprocessable_entity }
+        respond_to do |format|
+          if @message.save
+            format.json { render json: @message, status: :created }
+          else
+            format.json { render json: @message.errors, status: :unprocessable_entity }
+          end
+        end
+      end
+
+      def favorite
+        @message = Message.find(params[:id])
+        User.current.add_message_to_favorites(@message)
+
+        respond_to do |format|
+          format.json { render json: @message, status: :ok }
+        end
+      end
+
+      private
+
+      def message_params
+        params.require(:message).permit(:content, :user_id)
       end
     end
-  end
-
-  def favorite
-    @message = Message.find(params[:id])
-    User.current.add_message_to_favorites(@message)
-
-    respond_to do |format|
-      format.json { render json: @message, status: :ok }
-    end
-  end
-
-  private
-
-  def message_params
-    params.require(:message).permit(:content, :user_id)
   end
 end
